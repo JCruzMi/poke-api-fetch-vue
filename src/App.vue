7062572a0bca4ba33e91ec3d978063f9140cad2e
JCruzMi <47216265+JCruzMi@users.noreply.github.com>
2020-12-05 11:58:58 -0500

    start
    .

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,19 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <poke-app></poke-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import PokeApp from './components/PokeApp.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    PokeApp
+    
   }
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+
 </style>
